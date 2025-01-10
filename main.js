d7bd3110e58c4f9865879147259f8aa07e1c3679
Yusuf Daniel <123603604+Mammoman@users.noreply.github.com>
2025-01-11 00:38:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('active');
-});
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+const updateSliderPosition = () => {
+  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+};
+
+const showNextSlide = () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSliderPosition();
+};
+
+const showPrevSlide = () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSliderPosition();
+};
+
+nextBtn.addEventListener('click', showNextSlide);
+prevBtn.addEventListener('click', showPrevSlide);
+
